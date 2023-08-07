356cbe8e6e9a899ae78d7e200c9d5223c0614858
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-08-06 18:10:24 -0600

    se creo la v2

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    const module = {
+      cacheDom: function () {
+        this.functionalities = $('.hover');
+        this.cards = $('.BodyMain-card');
+        this.parrafo = $('.parrafo');
+        this.parrafo1 = $('.parrafo-1');
+        this.parrafo10 = $('.parrafo-1-0');
+      },
+  
+      main: function () {
+        this.cacheDom();
+        this.bindEvents();
+      },
+  
+      bindEvents: function () {
+        this.functionalities.each(function () {
+          $(this).on("mouseover", function () {
+            const motionShoes = $(this).find('.BodyMain-card-shoes');
+            motionShoes.addClass('BodyMain-card-efects');
+          });
+  
+          $(this).on("mouseout", function () {
+            const motionShoes = $(this).find('.BodyMain-card-shoes');
+            motionShoes.removeClass('BodyMain-card-efects');
+          });
+        });
+  
+        this.cards.each(function () {
+          const buttonsContainer = $(this).find('.buttons-container');
+          const parrafoVisible = $(this).find('.parrafo');
+          const parrafo1Visible = $(this).find('.parrafo-1');
+          const parrafo10Visible = $(this).find('.parrafo-1-0');
+  
+          $(this).on('mouseenter', () => {
+            buttonsContainer.addClass('active');
+            setTimeout(() => {
+              parrafoVisible.addClass('parrafo-visible');
+              parrafo1Visible.addClass('parrafo-1-visible');
+              parrafo10Visible.addClass('parrafo-1-0-visible');
+            }, 500); // Ajusta este valor según el tiempo de la transición de los botones
+          });
+  
+          $(this).on('mouseleave', () => {
+            buttonsContainer.removeClass('active');
+            parrafoVisible.removeClass('parrafo-visible');
+            parrafo1Visible.removeClass('parrafo-1-visible');
+            parrafo10Visible.removeClass('parrafo-1-0-visible');
+          });
+        });
+      },
+    };
+  
+    module.main();
+  });
+  
